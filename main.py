cbec7e2b77e518f10bb6f8b3deb1d73b8bbd151c
shPatrick <patrick.sculac@skole.hr>
2025-05-07 19:04:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 import pygame
 from sys import exit
 
-#pokretanje igrice, rezolucije i ime prozora
+#pokretanje igrice, rezolucije, ime prozora, pod
 pygame.init()
 screen=pygame.display.set_mode((1366,768))
 pygame.display.set_caption('2D maze runner')
 clock = pygame.time.Clock()
+
+test_pod = pygame.Surface((1366,768))
+test_pod.fill('bisque2')
 
 #ulazimo u beskonačnu petlju koja se može prekinuti samo iznutra
 while True:
@@ -14,7 +17,9 @@ while True:
         #interno zaustavljanje programa
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            exit()
+
+    screen.blit(test_pod,(0,0))        
     
     #konst. azuriranje ekrana
     pygame.display.update()
