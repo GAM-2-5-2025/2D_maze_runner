91c5b3620a6af0eca882c61c5289a809444eeab4
ViziblInvizibl <josip.sebelja321@gmail.com>
2025-05-21 19:46:20 +0200

    Dodan file za multiplayer
    - Multiplayer-  pokrenuta radnja multiplayera

---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from _thread import *
+import sys
+
+server = ""
+port = 5555
+
+s = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+        str(e)
+
+s.listen(25)
+print("Waiting for a connection, Server Started")
+
+def threaded_client(conn):
+
+    reply = ""
+    
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            
+
+
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
